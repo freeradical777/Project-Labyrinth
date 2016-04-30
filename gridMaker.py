777541d51a03ee6ac0965ef7c2e724bb7fd4d192
Christopher King <G.nius.ck@gmail.com>
2016-04-30 13:30:02 -0400

    Ascii Grid Viewer

--- gridMaker.py
+++ gridMaker.py
@@ -12,5 +12,20 @@ def newFullGrid(n,m):
             d[(i,j)] = [True,True,True,True]
     return d
 
+def asciiGrid(g,n,m):
+    c = ""
+    for i in xrange(n):
+        for j in xrange(m):
+            if g[(i,j)][down]:
+                c += '_'
+            else:
+                c += ' '
+            if g[(i,j)][right]:
+                c += '|'
+            else:
+                c += ' '
+        c += '\n'
+    return c
+
 def randomGrid(n,m):
     return newFullGrid(n,m)
