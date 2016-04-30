9c809390a4073846be87d965024b509cfa5f597b
Jacob Bender <benderjacob44@gmail.com>
2016-04-30 15:27:57 -0400

    Finished the minotaur function

--- labyrinth.py
+++ labyrinth.py
@@ -6,7 +6,7 @@ class maze(object):
     def __init__(self, grid):
         self.grid=grid
         self.player_pos = (0,0)
-        self.minotaur_pos = (10,10)
+        self.minotaur_pos = (9,9)
     def move_minotaur(self):
         moves=[(self.player_pos)]
         path_found = False
@@ -14,13 +14,24 @@ class maze(object):
         while not path_found:
             for move in moves: #For every cell in the plotted moves
                 cell = self.grid[move] #The attributes of that cell
+                dir_itr = 0
                 for direction in cell: #For every possible wall location
+                    
                     if direction != True: #If there is not a wall
-                        new_move=gridMaker.go(move, direction)
-                        moves.append(new_move) #Append the adjacent cell 
-                        if new_move == minotaur_pos: #If the new move reaches the minotaur, stop
-                            path_found=True
-                            self.minotaur_pos=new_move
+                        
+                        new_move=gridMaker.go(move, dir_itr)
+                        
+                        if new_move not in moves:
+                            moves.append(new_move) #Append the adjacent cell 
+                            if new_move == self.minotaur_pos: #If the new move reaches the minotaur, stop
+                                path_found=True
+                                self.minotaur_pos=move
+                                break
+                    dir_itr += 1
+                    if path_found == True:
+                        break
+                if path_found == True:
+                    break
 
 m=maze(grid)
 m.move_minotaur()
