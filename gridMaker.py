b876602bc8dff48c5ad8f14e30f5ec8ce69ad44d
Christopher King <G.nius.ck@gmail.com>
2016-04-30 13:49:59 -0400

    Random Grid thing

--- gridMaker.py
+++ gridMaker.py
@@ -4,6 +4,33 @@ up = 0
 left = 1
 down = 2
 right = 3
+dirs = [up,left,down,right]
+
+def neg(d):
+    if   d == up:
+        return down
+    elif d == left:
+        return right
+    elif d == down:
+        return up
+    elif d == right:
+        return left
+    else:
+        raise(ValueError, d ++ " is not a direction")
+        return do
+
+def go((i,j),d):
+    if   d == up:
+        return (i,j-1)
+    elif d == left:
+        return (i-1,j)
+    elif d == down:
+        return (i,j+1)
+    elif d == right:
+        return (i+1,j)
+    else:
+        raise(ValueError, d ++ " is not a direction")
+
 
 def newFullGrid(n,m):
     d = {}
@@ -28,4 +55,23 @@ def asciiGrid(g,n,m):
     return c
 
 def randomGrid(n,m):
-    return newFullGrid(n,m)
+    #From http://weblog.jamisbuck.org/2011/1/27/maze-generation-growing-tree-algorithm
+    g = newFullGrid(n,m)
+    c = [random.choice(g.keys())]
+    while c:
+        node = pickNode(c)
+        neighbors = []
+        for d in dirs:
+            neighbor = g.get(go(node,d), node)
+            if neighbor not in c:
+                neighbors.append((d,neighbor))
+        c.remove(node)
+        if not neighbors:
+            continue
+        (d,neighbor) = pickNeighbor(neighbors)
+        g[node][d] = False
+        g[neighbor][neg(d)] = False
+        c.append(neighbor)
+
+pickNode = random.choice
+pickNeighbor = random.choice
