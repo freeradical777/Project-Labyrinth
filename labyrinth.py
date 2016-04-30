769ba314fb46c4b0fad89f18a4c9ed8c176a25a7
Jacob Bender <benderjacob44@gmail.com>
2016-04-30 16:28:41 -0400

    Now checks if the minotaur is on top of the player

--- labyrinth.py
+++ labyrinth.py
@@ -21,27 +21,28 @@ class Maze(object):
     def won(self): return self.player_pos == self.star_pos
     def lost(self): return self.player_pos == self.minotaur_pos
     def move_minotaur(self):
-        moves=[(self.player_pos)]
-        path_found = False
+        if self.player_pos != self.minotaur_pos:
+            moves=[(self.player_pos)]
+            path_found = False
 
-        while not path_found:
-            for move in moves: #For every cell in the plotted moves
-                cell = self.grid[move] #The attributes of that cell
-                dir_itr = 0
-                for direction in cell: #For every possible wall location
-                    if direction != True: #If there is not a wall
-                        new_move=go(move, dir_itr)
-                        if new_move not in moves:
-                            moves.append(new_move) #Append the adjacent cell 
-                            if new_move == self.minotaur_pos: #If the new move reaches the minotaur, stop
-                                path_found=True
-                                self.minotaur_pos=move
-                                break
-                    dir_itr += 1
+            while not path_found:
+                for move in moves: #For every cell in the plotted moves
+                    cell = self.grid[move] #The attributes of that cell
+                    dir_itr = 0
+                    for direction in cell: #For every possible wall location
+                        if direction != True: #If there is not a wall
+                            new_move=go(move, dir_itr)
+                            if new_move not in moves:
+                                moves.append(new_move) #Append the adjacent cell 
+                                if new_move == self.minotaur_pos: #If the new move reaches the minotaur, stop
+                                    path_found=True
+                                    self.minotaur_pos=move
+                                    break
+                        dir_itr += 1
+                        if path_found == True:
+                            break
                     if path_found == True:
                         break
-                if path_found == True:
-                    break
 
     def move_player(self, key):
         start = self.grid[self.player_pos]
